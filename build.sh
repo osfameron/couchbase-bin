9cf337d5bd26ac892a84dbeb8ef8eae54ba30422
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-06-17 17:34:17 +0100

    BRANCHES variable

--- build.sh
+++ build.sh
@@ -5,19 +5,24 @@
 BASE=~/latest-antora-build
 PAT=$BASE.pattern
 LOG=$BASE.log
+BRANCHES=${BRANCHES:-\"HEAD\"}
 
 PLAYBOOK=lightweight-playbook.yml
 PLAYBOOK_MOD=lightweight-playbook-modified.yml
 
-git status -s | awk '{ print $2 }' > $PAT
+source $NVM_DIR/nvm.sh
+nvm use --lts
 
 pushd $(git rev-parse --show-toplevel)/ > /dev/null
+
+git status -s | awk '{ print $2 }' > $PAT
+
 PROJECT=$(basename $(pwd))
 NAME=$(yq eval .name antora.yml)
 
 pushd ../docs-site > /dev/null
 
-yq eval ".content.sources += [{\"url\": \"../${PROJECT}\", \"branches\": [\"HEAD\"]}]" $PLAYBOOK > $PLAYBOOK_MOD
+yq eval ".content.sources += [{\"url\": \"../${PROJECT}\", \"branches\": [$BRANCHES]}]" $PLAYBOOK > $PLAYBOOK_MOD
 
 echo "Building $PLAYBOOK_MOD for $PROJECT ($NAME) (see $LOG for full details)"
 echo "Using the pattern:"
@@ -27,13 +32,14 @@ PREV_SIZE=$(($(wc -c < $LOG) +0))
 
 echo "Previous size was $PREV_SIZE"
 
-antora $PLAYBOOK_MOD 2>&1 \
+
+antora --stacktrace $PLAYBOOK_MOD 2>&1 \
     | tee $LOG \
     | pv -s $PREV_SIZE \
-    | grep --color=auto --file $PAT --fixed-strings
+    | grep --color=auto --file $PAT --fixed-strings \
 
 echo
-echo Open file:$(pwd)/../docs-site/public/${NAME} to view docs
+echo Open file:$(pwd)/public/${NAME} to view docs
 
 popd > /dev/null
 popd > /dev/null
