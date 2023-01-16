996b88d4561c4195eb364e1193b1e410863094c2
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-01-16 17:06:04 +0000

    update scripts

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ PAT=$BASE.pattern
 LOG=$BASE.log
 BRANCHES=${BRANCHES:-\"HEAD\"}
 
-PLAYBOOK=lightweight-playbook.yml
-PLAYBOOK_MOD=lightweight-playbook-modified.yml
+PLAYBOOK=${PLAYBOOK:-lightweight-playbook.yml}
+PLAYBOOK_MOD=modified-${PLAYBOOK}
+
+EXTRA_ARGUMENTS=$*
 
 source $NVM_DIR/nvm.sh
 nvm use --lts
@@ -16,13 +18,26 @@ nvm use --lts
 pushd $(git rev-parse --show-toplevel)/ > /dev/null
 
 git status -s | awk '{ print $2 }' > $PAT
+git status -s | awk '{ print $2 }' | xargs -L1 basename >> $PAT
 
 PROJECT=$(basename $(pwd))
 NAME=$(yq eval .name antora.yml)
 
 pushd ../docs-site > /dev/null
 
-yq eval ".content.sources += [{\"url\": \"../${PROJECT}\", \"branches\": [$BRANCHES]}]" $PLAYBOOK > $PLAYBOOK_MOD
+cp $PLAYBOOK $PLAYBOOK_MOD
+
+echo CHECKING $PROJECT
+if [ "$PROJECT" != "docs-server" ]
+then
+	yq eval ".content.sources += [{\"url\": \"../${PROJECT}\", \"branches\": [$BRANCHES]}]" $PLAYBOOK_MOD > $PLAYBOOK_MOD.1
+	mv $PLAYBOOK_MOD.1 $PLAYBOOK_MOD
+fi
+
+yq eval ".asciidoc.attributes.primary-site-url = \"https://docs-staging.couchbase.com\"" $PLAYBOOK_MOD > $PLAYBOOK_MOD.1
+mv $PLAYBOOK_MOD.1 $PLAYBOOK_MOD
+
+cat $PLAYBOOK_MOD
 
 echo "Building $PLAYBOOK_MOD for $PROJECT ($NAME) (see $LOG for full details)"
 echo "Using the pattern:"
@@ -32,11 +47,19 @@ PREV_SIZE=$(($(wc -c < $LOG) +0))
 
 echo "Previous size was $PREV_SIZE"
 
+set -x
 
-antora --stacktrace $PLAYBOOK_MOD 2>&1 \
+pwd
+ls -ltr $PLAYBOOK_MOD
+
+antora $EXTRA_ARGUMENTS $PLAYBOOK_MOD 2>&1 \
     | tee $LOG \
-    | pv -s $PREV_SIZE \
-    | grep --color=auto --file $PAT --fixed-strings \
+    | pv -s $PREV_SIZE 
+# \
+#    | grep --file $PAT --fixed-strings \
+#    | jq
+
+set +x
 
 # Following requires `public` to be `git init`.
 # We don't need to save these, it just helps to do the diff of resulting output
